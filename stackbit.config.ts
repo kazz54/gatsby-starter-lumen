e65ad5c200ffd0f823741e2c9485998728b33c21
kazz54 <jonathan.mrutu@gmail.com>
2023-09-08 21:14:39 +0300

    Update stackbit.config.ts

--- stackbit.config.ts
+++ stackbit.config.ts
@@ -3,6 +3,7 @@ import { GitContentSource } from '@stackbit/cms-git'
 
 export default defineStackbitConfig({
   stackbitVersion: '~0.6.0',
+  nodeVersion: '18',
   contentSources: [
     new GitContentSource({
       rootPath: __dirname,
